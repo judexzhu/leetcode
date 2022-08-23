251031c5f988415a3f90435ea105db5ea51febe9
JudeXZhu <Jude.X.Zhu@gmail.com>
2022-08-22 22:18:14 -0700

    add 1

---
+++ 1.two-sum.go
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (49.00%)
+ * Likes:    36558
+ * Dislikes: 1162
+ * Total Accepted:    7.5M
+ * Total Submissions: 15.4M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
+ * 
+ * 
+ * 
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time
+ * complexity?
+ */
+package leetcode
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+
+	hashmap := make(map[int]int)
+
+	for i, num := range(nums) {
+		_, ok := hashmap[num]
+		if ok {
+			return []int{i, hashmap[num]}
+		}
+
+		hashmap[target - num] = i
+	}
+
+
+    return nil
+}
+// @lc code=end
+
