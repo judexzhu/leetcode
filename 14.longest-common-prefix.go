4ff9a190eb64febb726382e55b1096e3cfbba399
JudeXZhu <Jude.X.Zhu@gmail.com>
2022-08-25 13:32:02 -0700

    litte change on 14

--- 14.longest-common-prefix.go
+++ 14.longest-common-prefix.go
@@ -53,17 +53,12 @@ func longestCommonPrefix(strs []string) string {
 	// First, sort the string slice
 	sort.Strings(strs)
 
+	// After sort. we only need compare the first word with the last word
+	first_word := strs[0]	
+	last_word := strs[len(strs)-1]
 	prefix := ""
 
-	n := len(strs)
-	// After sort. we only need compare the first word with the last word
-	first_word := strs[0]
-	
-	last_word := strs[n-1]
-
-	length := len(first_word)
-
-	for i:=0; i<length; i++ {
+	for i:=0; i<len(first_word); i++ {
 		// compare start from the first char of first word and last word, if same, try next char. once diff, break the for loop.
 		if first_word[i] == last_word[i]{
 			prefix = prefix + string(first_word[i])
