f0472c3b3e2618b57e459b62caad15a4d4a72b42
JudeXZhu <Jude.X.Zhu@gmail.com>
2022-10-15 19:00:25 -0700

    572, DFS base on problem 100 isSameTree

---
+++ 572.subtree-of-another-tree.go
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=572 lang=golang
+ *
+ * [572] Subtree of Another Tree
+ *
+ * https://leetcode.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (45.74%)
+ * Likes:    6298
+ * Dislikes: 346
+ * Total Accepted:    566.9K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * Given the roots of two binary trees root and subRoot, return true if there
+ * is a subtree of root with the same structure and node values of subRoot and
+ * false otherwise.
+ * 
+ * A subtree of a binary tree tree is a tree that consists of a node in tree
+ * and all of this node's descendants. The tree tree could also be considered
+ * as a subtree of itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the root tree is in the range [1, 2000].
+ * The number of nodes in the subRoot tree is in the range [1, 1000].
+ * -10^4 <= root.val <= 10^4
+ * -10^4 <= subRoot.val <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	// DFS
+	// Base on problem 100 isSameTree
+
+	if root == nil {
+		return subRoot == nil 
+	}
+
+	if isSameTree(root, subRoot) {
+		return true
+	}
+    
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} 
+
+	if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+
+}
+// @lc code=end
+
